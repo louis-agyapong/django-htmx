2b2c39d16a7cefdecf8e568fe737da56689c0321
Louis Agyapong <agyapong.louis@gmail.com>
2021-12-22 10:00:03 +0000

    film model

--- apps/films/models.py
+++ apps/films/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
@@ -6,3 +8,8 @@ class User(AbstractUser):
 
     class Meta:
         app_label = "films"
+
+
+class Film(models.Model):
+    name = models.CharField(_("name"), max_length=128, unique=True)
+    users = models.ManyToManyField(User, verbose_name=_("users"), related_name="films")
